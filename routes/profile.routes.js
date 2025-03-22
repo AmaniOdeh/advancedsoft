b191012d0f414f981f836b57da1bfd627c1f141b
AmaniOdeh <amaniodeh225@gmail.com>
2025-03-22 23:11:23 +0200

    Initial push to GitHub454

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const profileController = require("../controllers/profile.controller");
 const { authenticateJWT } = require("../middleware/auth.middleware");
 
-router.get("/me", authenticateJWT, profileController.getProfile);
+router.get("/", authenticateJWT, profileController.getProfile);
+router.put("/", authenticateJWT, profileController.updateProfile);
+router.delete("/", authenticateJWT, profileController.deleteProfile);
 
 module.exports = router;
