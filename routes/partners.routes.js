ce90dbb33ed768a43e82beff5ecbc2298a8aba10
AbdAlbasetRajab2024 <S12042362@stu.najah.edu>
2025-05-10 13:51:16 +0300

    partners&tranactions

--- routes/partners.routes.js
+++ routes/partners.routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const partnerController = require('../controllers/partner.controller');
+const { authenticateJWT, authorizeRoles } = require("../middleware/auth.middleware");
 
-router.post('/', partnerController.addPartner);
-router.get('/', partnerController.getAllPartners);
+router.post('/',authenticateJWT, authorizeRoles("admin"), partnerController.addPartner);
+router.get('/',authenticateJWT, authorizeRoles("admin"), partnerController.getAllPartners);
 
 module.exports = router;
--- routes/transactions.routes.js
+++ routes/transactions.routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const transactionController = require('../controllers/transaction.controller');
+const { authenticateJWT, authorizeRoles } = require("../middleware/auth.middleware");
 
-router.post('/:donationId', transactionController.processTransaction);
-router.get('/', transactionController.getAllTransactions);
+router.post('/:donationId', authenticateJWT, authorizeRoles("admin"), transactionController.processTransaction);
+router.get('/',authenticateJWT,  authorizeRoles("admin"), transactionController.getAllTransactions);
 
 module.exports = router;
