9ecf8e1dfa1b23af2de3b60d2776d371c39c2958
AbdAlbasetRajab2024 <S12042362@stu.najah.edu>
2025-05-11 10:34:16 +0300

    change transactions (from admin to orphanage)

--- routes/partners.routes.js
+++ routes/partners.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const partnerController = require('../controllers/partner.controller');
 const { authenticateJWT, authorizeRoles } = require("../middleware/auth.middleware");
 
-router.post('/',authenticateJWT, authorizeRoles("admin"), partnerController.addPartner);
-router.get('/',authenticateJWT, authorizeRoles("admin"), partnerController.getAllPartners);
+router.post('/',authenticateJWT, authorizeRoles("orphanage"), partnerController.addPartner);
+router.get('/',authenticateJWT, authorizeRoles("orphanage"), partnerController.getAllPartners);
 
 module.exports = router;
--- routes/transactions.routes.js
+++ routes/transactions.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const transactionController = require('../controllers/transaction.controller');
 const { authenticateJWT, authorizeRoles } = require("../middleware/auth.middleware");
 
-router.post('/:donationId', authenticateJWT, authorizeRoles("admin"), transactionController.processTransaction);
-router.get('/',authenticateJWT,  authorizeRoles("admin"), transactionController.getAllTransactions);
+router.post('/:donationId', authenticateJWT, authorizeRoles("orphanage"), transactionController.processTransaction);
+router.get('/',authenticateJWT,  authorizeRoles("orphanage"), transactionController.getAllTransactions);
 
 module.exports = router;
